33c708f565519e0b2a9a4d97a9f6d87256e6d935
zrj <2112329899@qq.com>
2026-09-01 11:10:59 +0800

    添加时区配置

---
+++ files/run.sh
@@ -0,0 +1,42 @@
+cloud-provider-payment
+cloud-consumer-order
+cloud-provider-payment-8002
+
+docker ps
+
+docker start nacos_v2_now
+
+mvn dependency:tree
+
+
+
+getent hosts github.com
+# 127.0.0.1       github.com
+
+
+"""
+[wsl2]
+dnsTunneling=true
+autoProxy=true
+networkingMode=mirrored
+""" 
+# >> %USERPROFILE%\.wslconfig
+# >> ~/.wslconfig
+
+cmd "code %USERPROFILE%\.wslconfig"
+# 镜像模式无需配置git代理
+nc -vz github.com 443
+cat /etc/hosts
+
+cat /etc
+cat /etc/resolv.conf
+cat /etc/hosts
+
+curl -s "https://get.sdkman.io" | bash
+
+
+
+sdk install java 26.0.2+1.1-tem
+sdk default maven 3.9.16
+
+
